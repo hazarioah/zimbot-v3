e0ccb45fb43b5fe8be9ddcb5202a00ad3b3c72f1
hazarioah <107625668+hazarioah@users.noreply.github.com>
2022-06-28 18:03:41 +0530

    Update config.js
    Signed-off-by: hazarioah <107625668+hazarioah@users.noreply.github.com>

--- config.js
+++ config.js
@@ -37,10 +37,10 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['27634090203','']
-global.premium = ['27634090203','918188019676']
-global.botname = 'ZIM BOT INC'
-global.ownername = 'DRIPS'
+global.owner = ['916282888139']
+global.premium = ['916282888139']
+global.botname = 'Afyoon'
+global.ownername = 'Afyoon'
 global.group1 = 'https://chat.whatsapp.com/EFsb8RCXV4jLEFk4eAcA1A'
 global.group2 = 'https://chat.whatsapp.com/HSfcYU13g5C8GxINWwSWoA'
 global.packname = 'ZIM BOT INC'
